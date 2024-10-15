0045ff73b1ac68cadd2da87804637499e20290d4
Zhu Yuchen <3062689042@qq.com>
2024-10-16 04:36:58 +0800

    Modify LoginView

--- Frontend/open-course-website/src/views/LoginView.vue
+++ Frontend/open-course-website/src/views/LoginView.vue
@@ -1,8 +1,99 @@
+<template>
+  <div class="center">
+    <h1 style="font-size: 30px; color: black;">Sign in/ Sign up</h1>
+    <div class="container">
+      <div :class="mainPanel">
+
+        <div class="form-style" v-if="status == 'signIn'">
+          <h2 style="font-size: 25px; color: black">Sign in</h2>
+          <div>
+            <el-icon><User /></el-icon>
+            <input type="text" placeholder="Account" style="margin-left: 10px;">
+          </div>
+          <div>
+            <el-icon color="black"><Lock /></el-icon>
+            <input type="text" placeholder="Password" style="margin-left: 10px;">
+          </div>
+          <h3 style="font-size: 12px">Forgot your password?</h3>
+          <button class="submit-button" @click="signIn">Sign in</button>
+        </div>
+
+        <div class="form-style" v-if="status == 'signUp'">
+          <h2 style="font-size: 25px; color: black">Registered Account</h2>
+          <div>
+            <el-icon><User /></el-icon>
+            <input type="text" placeholder="Account" style="margin-left: 10px;">
+          </div>
+          <div>
+            <el-icon color="black"><Lock /></el-icon>
+            <input type="text" placeholder="Password" style="margin-left: 10px;">
+          </div>
+          <button class="submit-button" @click="signUp">Sign up</button>
+        </div>
+
+      </div>
+      <div :class="overlay">
+        <div class="form-style" v-if="status == 'signIn'">
+          <h2 class="overlaytitleH2">Welcome to SUSTech Open Course Website!</h2>
+          <p class="overlaytitleP">
+            Don't have an account?
+          </p>
+          <p class="overlaytitleP">
+            Sign up and start journey now!
+          </p>
+          <div class="switch-button" @click="switchToSignUpPanel">Register</div>
+        </div>
+        <div class="form-style" v-if="status == 'signUp'">
+          <h2 class="overlaytitleH2">Welcome Back!</h2>
+          <p class="overlaytitleP">To keep connected with us please login with your personal info</p>
+          <div class="switch-button" @click="switchToSignInPanel">Sign in</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      mainPanel: 'mainPanel-signIn',
+      overlay: 'overlay-signUp',
+      status: 'signIn'
+    }
+  },
+  methods: {
+    switchToSignUpPanel() {
+      console.log("Switch to sign up panel")
+      this.mainPanel = "mainPanel-signUp"
+      this.overlay = "overlay-signIn"
+      setTimeout(() => {
+        this.status = 'signUp'
+      }, 200)
+    },
+    switchToSignInPanel() {
+      console.log("Switch to sign in panel")
+      this.mainPanel = "mainPanel-signIn"
+      this.overlay = "overlay-signUp"
+      setTimeout(() => {
+        this.status = 'signIn'
+      }, 200)
+    },
+    signIn(){
+      alert('登录成功！');
+    },
+    signUp(){
+      alert('注册成功！');
+    },
+  }
+}
+</script>
+
 <style>
 .center {
   width: 1920px;
   height: 1080px;
-  //background-image: url('https://gd-hbimg.huaban.com/9165dc3c0f8279ae5402d1f47212847bff68b49f3cb547-QEli8R');
   background-size: 100% 100%;
   background-repeat: no-repeat;
   display: flex;
@@ -22,7 +113,7 @@
   min-height: 480px;
   margin-top: 20px;
   display: flex;
-  background: -webkit-linear-gradient(90deg, #4284db, #29eac4);
+  background: linear-gradient(135deg, #292a3a, #536976);
 }
 
 .mainPanel-signUp {
@@ -50,9 +141,11 @@
 }
 
 .overlaytitleH2 {
-  font-size: 30px;
+  font-size: 25px;
   color: #fff;
   margin-top: 20px;
+  margin-left: 10px;
+  margin-right: 10px;
 }
 
 .overlaytitleP {
@@ -93,7 +186,7 @@
   flex-direction: column;
 }
 
-.buttongohs {
+.switch-button {
   width: 180px;
   height: 40px;
   border-radius: 50px;
@@ -103,6 +196,12 @@
   text-align: center;
   line-height: 40px;
   margin-top: 40px;
+  cursor: pointer;
+  transition: .3s;
+}
+
+.switch-button:hover {
+  transform: scale(1.1);
 }
 
 .overlaylongH2 {
@@ -123,6 +222,7 @@ h3{
   margin-top: 10px;
   cursor: pointer;
 }
+
 .inupbutton{
   background-color: #29eac4;
   border: none;
@@ -134,74 +234,56 @@ h3{
   text-align: center;
   line-height: 40px;
   margin-top: 30px;
+  cursor: pointer;
 }
-</style>
 
-<template>
-  <div class="center">
-    <h1 style="font-size: 30px; color: black;">Sign in/ Sign up</h1>
-    <div class="container">
-      <div :class="overlaylong">
-        <div class="form-style" v-if="status == 'signIn'">
-          <h2 class="overlaylongH2">Sign in</h2>
-          <input type="text" placeholder="user">
-          <input type="text" placeholder="password">
-          <h3>Forgot your password?</h3>
-          <button class="inupbutton">Sign in</button>
-        </div>
-        <div class="form-style" v-if="status == 'signUp'">
-          <h2 class="overlaylongH2">Registered Account</h2>
-          <input type="text" placeholder="user">
-          <input type="text" placeholder="password">
-          <button class="inupbutton">Sign up</button>
-        </div>
-      </div>
-      <div :class="overlaytitle">
-        <div class="form-style" v-if="status == 'signIn'">
-          <h2 class="overlaytitleH2">Hello,Friend!</h2>
-          <p class="overlaytitleP">
-            Enter your personal details and start journey with us
-          </p>
-          <div class="buttongohs" @click="Signin">Sign up</div>
-        </div>
-        <div class="form-style" v-if="status == 'signUp'">
-          <h2 class="overlaytitleH2">Welcome Back!</h2>
-          <p class="overlaytitleP">To keep connected with us please login with your personal info</p>
-          <div class="buttongohs" @click="Signup">Sign in</div>
-        </div>
-      </div>
-    </div>
+.submit-button {
+  margin-left: 60%;
+  margin-top: 50px;
+  width: 180px;
+  height: 40px;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  line-height: 40px;
+  color: #fff;
+  font-size: 15px;
+  text-decoration: none;
+  font-family: sans-serif;
+  box-sizing: border-box;
+  background: linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4);
+  border: transparent;
+  border-radius: 50px;
+  background-size: 400%;
+  cursor: pointer;
+  z-index: 1;
+}
 
-  </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      overlaylong: 'mainPanel-signIn',
-      overlaytitle: 'overlay-signUp',
-      status: 'signIn'
-    }
-  },
-  methods: {
-    Signin() {
-      console.log("Invoke the sign in function")
-      this.overlaylong = "mainPanel-signUp"
-      this.overlaytitle = "overlay-signIn"
-      setTimeout(() => {
-        this.status = 'signUp'
-      }, 200)
-    },
-    Signup() {
-      console.log("Invoke the sign up function")
-      this.overlaylong = "mainPanel-signIn"
-      this.overlaytitle = "overlay-signUp"
-      setTimeout(() => {
-        this.status = 'signIn'
-      }, 200)
-
-    }
+.submit-button:hover {
+  animation: animate 8s linear infinite;
+}
+@keyframes animate{
+  from{
+    background-position: 0%;
+  }
+  to {
+    background-position: 400%;
   }
 }
-</script>
+.submit-button::before{
+  content: '';
+  position: absolute;
+  inset: 0px;
+  z-index: -1;
+  background: linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4);
+  background-size: 400%;
+  border-radius: 5px;
+  opacity: 0;
+}
+.submit-button:hover::before{
+  filter: blur(10px);
+  opacity: 1;
+  animation: animate 8s linear infinite;
+}
+
+
+</style>
